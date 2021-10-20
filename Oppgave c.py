787fdfa7160d7d4877ee0b033df4347829692971
Mcmar279 <marius.ree02@gmail.com>
2021-10-20 20:45:03 +0200

    Test

--- Oppgave c.py
+++ Oppgave c.py
@@ -5,20 +5,39 @@ Created on Mon Oct 11 10:16:00 2021
 @author: mariu
 """
 
-spørsmål = ["Hvor mange minutt er det i en time?", "Hva er 2+2?"]
-mulige_svar = [["1: 15", "2: 30", "3: 45", "4: 60"], ["1: 15", "2: 4", "3: 96", "4: Pi"]]
+import re
 
+spørsmål = []
+mulige_svar = []
+riktig_svar = []
+
+
+
+with open("C:/Users/mariu/Documents/Git Repository/Innlevering_9_DAT120/sporsmaalsfil.txt", "r", encoding="UTF-8") as fil:
+    for row in fil:
+        new_list = re.split(":", row)
+#        print(new_list)
+        spørsmål.append(new_list[0])
+        riktig_svar.append(new_list[1].replace(" ",""))
+        mulige_svar.append((new_list[2]).strip())
 class flervalgsspørsmål():
     
-    def sjekk_svar(gjett):
-        
-        if spmNR == 0 and gjett == 4:
-            return "Det er riktig"
-        elif spmNR == 1 and gjett == 2:
-            return "Det er riktig"
+    def sjekk_svar(gjett1, gjett2, spiller1_score, spiller2_score):
+        if gjett1 == riktig_svar[spmNR] and gjett2 == riktig_svar[spmNR]:
+            spiller1_score += 1
+            spiller2_score += 1
+            return "Begge hadde rett"
+        elif gjett2 == riktig_svar[spmNR] and gjett1 != riktig_svar[spmNR]:
+            spiller2_score += 1
+            return "Spiller 1: Feil \nSpiller2: Riktig"
+        elif gjett1 == riktig_svar[spmNR] and gjett2 != riktig_svar[spmNR]:
+            spiller1_score += 1
+            return "Spiller 1: Riktig \nSpiller 2: Feil"
         else:
-            return "Det er feil"
-                
+            return "Begge hadde feil"
+    
+    def korrekt_svar_tekst(self):
+        return str(riktig_svar[self])
     
     def __init__(self, spmNR):
         self.tekst = spørsmål[spmNR]
@@ -29,8 +48,18 @@ class flervalgsspørsmål():
 
 if __name__ == "__main__":
     spmNR = 0
-    for i in range(2):
+    spiller1_score = 0
+    spiller2_score = 0
+    for i in range(len(spørsmål)):
         print(flervalgsspørsmål(spmNR))
-        gjett = int(input("\nHvilket tror du er riktig? "))
-        print(flervalgsspørsmål.sjekk_svar(gjett))
+        gjett1 = str(input("\nHvilket tror du er riktig? (Spiller 1) "))
+        gjett2 = str(input("\nHvilket tror du er riktig? (Spiller 2) "))
+        print("Det riktige svaret er:", flervalgsspørsmål.korrekt_svar_tekst(spmNR))
+        print(flervalgsspørsmål.sjekk_svar(gjett1, gjett2, spiller1_score, spiller2_score))
         spmNR += 1
+    print("Spiller 1 score:", spiller1_score)
+    print("Spiller 2 score:", spiller2_score)
+
+#for k in range(len(mulige_svar[spmNR])):
+#            for element in mulige_svar[spmNR]:
+#                print(mulige_svar[spmNR].index(element),":", element)
