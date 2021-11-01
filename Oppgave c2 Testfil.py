0ead9a6955542e5fb332d14f1f0e521894d09717
Mcmar279 <marius.ree02@gmail.com>
2021-11-01 10:45:52 +0100

    Endret c og c2

--- Oppgave c2 Testfil.py
+++ Oppgave c2 Testfil.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Oct 28 08:43:18 2021
-
 @author: mariu
 """
 
@@ -12,6 +11,8 @@ spørsmål = []
 mulige_svar = []
 riktig_svar = []
 spill_scenarioer = []
+score1 = []
+score2 = []
 
 class flervalgsspørsmål:
     
@@ -21,15 +22,47 @@ class flervalgsspørsmål:
         self.spiller1_score = 0
         self.spiller2_score = 0
     
-    def sjekk_svar():
-        return True
+    def sjekk_svar(self, gjett1, gjett2):
+        #Sjekker hva det korrekte svaret er
+        sammenlign = self.korrekt_svar_tekst()
+        print("Det riktige svaret er index", sammenlign)
+        if gjett1 == sammenlign and gjett2 == sammenlign:
+#            self.spiller1_score += 1
+#            self.spiller2_score += 1
+            score1.append(1)
+            score2.append(1)
+            return "Begge hadde rett"
+        elif gjett1 == sammenlign and gjett2 != sammenlign:
+#            self.spiller1_score += 1
+            score1.append(1)
+            # self.spiller2_score += 0
+            return "Spiller 1: Riktig \nSpiller 2: Feil"
+        elif gjett1 != sammenlign and gjett2 == sammenlign:
+            # self.spiller1_score += 0
+#            self.spiller2_score += 1
+            score2.append(1)
+            return "Spiller 1: Feil \nSpiller2: Riktig"
+        else:
+            print("Gjett1:", gjett1)
+            print("Gjett2:", gjett2)
+            return "Begge hadde feil"
 
     def korrekt_svar_tekst(self):
+        #legger til 1 for bedre brukervennlighet
         return int(rkt) + 1
     
     def __str__(self):
         return f"{self.tekst} \nAlternativene dine er {self.alternativ}"
 
+#rekkefølgen spillet spilles
+def spillrekkefølge():
+    #Printer "__str__"
+    print(flervalgsspørsmål(spm, rkt, mlg))
+    gjett1 = int(input("\nHvilket tror du er riktig? (Spiller 1) "))
+    gjett2 = int(input("\nHvilket tror du er riktig? (Spiller 2) "))
+    #Sammenligner med svaret
+    print(flervalgsspørsmål(spm, rkt, mlg).sjekk_svar(gjett1, gjett2))
+    pass
 
 with open("sporsmaalsfil.txt", "r", encoding="UTF-8") as fil:
     for row in fil:
@@ -38,21 +71,14 @@ with open("sporsmaalsfil.txt", "r", encoding="UTF-8") as fil:
         spm = new_list[0]
         rkt = new_list[1].replace(" ", "")
         mlg = new_list[2].strip()
+        #Liste med alle spill objekter
         spill_scenarioer.append(flervalgsspørsmål(spm, rkt, mlg))
-        
+        spillrekkefølge()
+
 if __name__ == "__main__":
-#    for i in range(len(spørsmål)):
-##        spill = flervalgsspørsmål(spm, rkt, mlg)
-##        gjett1 = int(input("\nHvilket tror du er riktig? (Spiller 1) "))
-##        gjett1 = gjett1 - 1
-##        gjett1 = str(gjett1)
-##        gjett2 = int(input("\nHvilket tror du er riktig? (Spiller 2) "))
-##        gjett2 = gjett2 - 1
-##        gjett2 = str(gjett2)
-##        print("Det riktige svaret er:", spill.korrekt_svar_tekst())
-#        print(spill.sjekk_svar(gjett1, gjett2))
-#        spmNR += 1
+    spill = flervalgsspørsmål(spm, rkt, mlg)
 #    print("Spiller 1 score:", spill.spiller1_score)
 #    print("Spiller 2 score:", spill.spiller2_score)
-    print(spill_scenarioer[0])
+    print("Spiller 1 score:", len(score1))
+    print("Spiller 2 score:", len(score2))
     pass
